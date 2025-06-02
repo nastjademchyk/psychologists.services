a9f4a2f96cb9e1e6d7e066b9fb5a1e126c8f7462
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-06-02 14:59:40 +0200

    added formik for filter on Psychologists page

--- src/pages/Psychologists/Psychologists.jsx
+++ src/pages/Psychologists/Psychologists.jsx
@@ -1,6 +1,29 @@
+import { Field, Form, Formik } from "formik";
 import s from "./Psychologists.module.css";
+import { useId } from "react";
 const Psychologists = () => {
-  return <div>Here will be page with Psychologists list </div>;
+  const filterId = useId();
+
+  return (
+    <div>
+      <Formik>
+        <Form className={s.form}>
+          <label htmlFor={filterId} className={s.text}>
+            Filters
+          </label>
+          <Field as="select" name="filter" className={s.filter} id={filterId}>
+            <option value="a-z">A to Z</option>
+            <option value="z-a">Z to A</option>
+            <option value="lt-10">Less than 10$</option>
+            <option value="gt-10">Greater than 10$</option>
+            <option value="popular">Popular</option>
+            <option value="not-popular">Not popular</option>
+            <option value="all">Show all</option>
+          </Field>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default Psychologists;
