4d8adeebc814082e5820f7a95e86452d5772fcec
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-06-02 19:44:27 +0200

    added arrow to the get started button

--- src/components/GetStartBtn/GetStartBtn.jsx
+++ src/components/GetStartBtn/GetStartBtn.jsx
@@ -1,9 +1,13 @@
 import s from "./GetStartBtn.module.css";
+import sprite from "../../assets/icons.svg";
 
 const GetStartBtn = ({ onClick }) => {
   return (
     <button onClick={onClick} className={s.btn} type="button">
       Get started
+      <svg width="28" height="28" className={s.icon}>
+        <use href={`${sprite}#icon-arrow-up-right2`} />
+      </svg>
     </button>
   );
 };
