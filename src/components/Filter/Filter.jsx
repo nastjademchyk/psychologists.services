1ebed5c1856f1c317f37712d32525091c13aed3a
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-06-08 16:20:27 +0200

    corrected styles for filter element

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,46 +1,116 @@
 import { Formik, Form, Field } from "formik";
 import { useId } from "react";
 import s from "./Filter.module.css";
+import Select, { components } from "react-select";
+import sprite from "../../assets/icons.svg";
+
+const options = [
+  { value: "a-z", label: "A to Z" },
+  { value: "z-a", label: "Z to A" },
+  { value: "lt-10", label: "Less than 10$" },
+  { value: "gt-10", label: "Greater than 10$" },
+  { value: "popular", label: "Popular" },
+  { value: "not-popular", label: "Not popular" },
+  { value: "all", label: "Show all" },
+];
+
+const DropdownIndicator = (props) => {
+  const { menuIsOpen } = props.selectProps;
+
+  return (
+    <components.DropdownIndicator {...props}>
+      {menuIsOpen ? (
+        <svg width="24" height="24" className={s.icon}>
+          <use href={`${sprite}#icon-chevron-up`} />
+        </svg>
+      ) : (
+        <svg width="24" height="24" className={s.icon}>
+          <use href={`${sprite}#icon-chevron-down`} />
+        </svg>
+      )}
+    </components.DropdownIndicator>
+  );
+};
 
 const Filter = ({ onChange }) => {
   const filterId = useId();
 
   return (
     <Formik
-      initialValues={{ filter: "all" }}
-      onSubmit={(values) => {
-        if (onChange) {
-          onChange(values.filter);
-        }
-      }}
+      initialValues={{ filter: options.find((opt) => opt.value === "all") }}
+      onSubmit={() => {}}
     >
-      {({ values, setFieldValue }) => (
+      {({ setFieldValue, values }) => (
         <Form className={s.form}>
           <label htmlFor={filterId} className={s.text}>
             Filters
           </label>
-          <Field
-            as="select"
-            name="filter"
-            className={s.filter}
+          <Select
             id={filterId}
+            isSearchable={false}
+            name="filter"
             value={values.filter}
-            onChange={(e) => {
-              const selectedValue = e.target.value;
-              setFieldValue("filter", selectedValue);
+            options={options}
+            classNamePrefix="custom-select"
+            components={{ DropdownIndicator }}
+            onChange={(selectedOption) => {
+              setFieldValue("filter", selectedOption);
               if (onChange) {
-                onChange(selectedValue);
+                onChange(selectedOption.value);
               }
             }}
-          >
-            <option value="a-z">A to Z</option>
-            <option value="z-a">Z to A</option>
-            <option value="lt-10">Less than 10$</option>
-            <option value="gt-10">Greater than 10$</option>
-            <option value="popular">Popular</option>
-            <option value="not-popular">Not popular</option>
-            <option value="all">Show all</option>
-          </Field>
+            styles={{
+              control: (base) => ({
+                ...base,
+                borderRadius: "14px",
+                backgroundColor: "#54be96",
+                padding: "4px",
+                height: "48px",
+                width: "226px",
+                border: "none",
+                boxShadow: "none",
+                cursor: "pointer",
+              }),
+              singleValue: (base) => ({
+                ...base,
+                color: "#fbfbfb",
+                fontWeight: 500,
+                fontSize: "16px",
+              }),
+              option: (base, state) => ({
+                ...base,
+                backgroundColor: "transparent",
+                color: state.isSelected ? "#191a15" : "rgba(25, 26, 21, 0.3)",
+                fontWeight: 400,
+                fontSize: "16px",
+                lineHeight: "125%",
+                cursor: "pointer",
+              }),
+              menuList: (base) => ({
+                ...base,
+                maxHeight: "216px",
+                overflowY: "auto",
+              }),
+              menu: (base) => ({
+                ...base,
+                borderRadius: "14px",
+                marginTop: 8,
+                width: "226px",
+                height: " 216px",
+                zIndex: 10,
+                boxShadow: "0 20px 69px 0 rgba(0, 0, 0, 0.07)",
+                backgroundColor: "#fff",
+              }),
+              dropdownIndicator: (base) => ({
+                ...base,
+                color: "#fbfbfb",
+              }),
+              indicatorSeparator: () => ({
+                display: "none",
+                cursor: "pointer",
+              }),
+            }}
+          />
         </Form>
       )}
     </Formik>
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -11,6 +11,7 @@ import { Link } from "react-router-dom";
 const HomePage = () => {
   return (
     <div className={s.container}>
+      {/* <div className={s.blur}></div> */}
       <div className={s.left}>
         <h1 className={s.header}>
           The road to the <span className={s.accent}>depths</span> of the human
