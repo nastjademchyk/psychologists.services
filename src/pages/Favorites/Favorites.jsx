918f1ec30a9d354814bf2a1c976ab85698870416
Nastja Demchyk <NastjaDemchyk@gmail.com>
2025-06-09 17:26:41 +0200

    added load more btn for favorites page

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,5 @@
 import { useSelector } from "react-redux";
 import { selectPsychologists } from "../../redux/psychologists/selectors";
-import PsychologistCard from "../../components/PsychologistCard/PsychologistCard";
 import s from "./Favorites.module.css";
 import Filter from "../../components/Filter/Filter";
 import { selectSortBy } from "../../redux/filters/selectors";
@@ -9,6 +8,7 @@ import { useEffect, useState } from "react";
 import { selectUser } from "../../redux/auth/selectors";
 import Appointment from "../../components/Appointment/Appointment";
 import Modal from "../../components/Modal/Modal";
+import PsychologistsList from "../../components/PsychologistsList/PsychologistsList";
 
 const Favorites = ({ onClose }) => {
   const allPsychologists = useSelector(selectPsychologists);
@@ -18,6 +18,7 @@ const Favorites = ({ onClose }) => {
   const [expandedIndexes, setExpandedIndexes] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
   const [selectedPsychologist, setSelectedPsychologist] = useState(null);
+  const [visibleCount, setVisibleCount] = useState(3);
 
   useEffect(() => {
     if (user && user.email) {
@@ -53,21 +54,19 @@ const Favorites = ({ onClose }) => {
   return (
     <div className={s.wrapper}>
       <Filter />
-      {favoritePsychologists.length === 0 ? (
+      {filteredFavorites.length === 0 ? (
         <p className={s.message}>You have no favorite psychologists yet.</p>
       ) : (
-        <ul className={s.list}>
-          {filteredFavorites.map((psychologist, index) => (
-            <PsychologistCard
-              key={psychologist.name}
-              psychologist={psychologist}
-              isExpanded={expandedIndexes.includes(index)}
-              onToggle={() => handleToggle(index)}
-              onOpenModal={() => handleOpenModal(psychologist)}
-              psychologistIndex={index}
-            />
-          ))}
-        </ul>
+        <PsychologistsList
+          psychologists={filteredFavorites}
+          visibleCount={visibleCount}
+          expandedIndexes={expandedIndexes}
+          onToggle={handleToggle}
+          onOpenModal={handleOpenModal}
+          onLoadMore={() => setVisibleCount((prev) => prev + 3)}
+          showLoadMore={true}
+          isLoading={false}
+        />
       )}
       {modalOpen && selectedPsychologist && (
         <Modal onClose={handleCloseModal}>
